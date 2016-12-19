5ce7481106b4f4502fc84ade4577dc5042f31a60
gentcys <iamchencong@gmail.com>
2016-12-19 12:16:48 +0800

    Add filename option for download

--- table2CSV.js
+++ table2CSV.js
@@ -5,7 +5,7 @@ jQuery.fn.table2CSV = function(options) {
         headerSelector: 'th',
         columnSelector: 'td',
         delivery: 'popup', // popup, value, download
-        // filename: 'powered_by_sinri.csv', // filename to download
+        filename: 'table2csv.csv', // filename to download
         transform_gt_lt: true // make &gt; and &lt; to > and <
     },
     options);
@@ -46,12 +46,18 @@ jQuery.fn.table2CSV = function(options) {
         return popup(mydata);
     }
     else if(options.delivery == 'download') {
-        var mydata = csvData.join('\n');
+        var mydata = csvData.join('\n'), a;
         if(options.transform_gt_lt){
             mydata=sinri_recover_gt_and_lt(mydata);
         }
         var url='data:text/csv;charset=utf8,' + encodeURIComponent(mydata);
-        window.open(url);
+        a = document.createElement("a");
+        a.download = options.filename;
+        a.href = url;
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        
         return true;
     } 
     else {
